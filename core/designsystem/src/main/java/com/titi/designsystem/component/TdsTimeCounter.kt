e3f704890d21ff9506f25986dc158c59f030e2dc
koreatlwls <koreatlwls@gmail.com>
2023-10-04 00:00:20 +0900

    update : tdstext 파라미터 tdscolor, tdstextstyle at tdsanimatedcounter, tdstimecounter

--- core/designsystem/src/main/java/com/titi/designsystem/component/TdsTimeCounter.kt
+++ core/designsystem/src/main/java/com/titi/designsystem/component/TdsTimeCounter.kt
@@ -3,10 +3,11 @@ package com.titi.designsystem.component
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.titi.designsystem.theme.TdsColor
+import com.titi.designsystem.theme.TdsTextStyle
 import com.titi.designsystem.theme.TiTiTheme
 
 data class TdsTime(
@@ -19,34 +20,40 @@ data class TdsTime(
 fun TdsTimeCounter(
     modifier: Modifier = Modifier,
     tdsTime: TdsTime,
-    color: Color,
-    textStyle: TextStyle
+    color: TdsColor,
+    textStyle: TdsTextStyle,
+    fontSize: TextUnit,
 ) {
     Row(modifier = modifier) {
         TdsAnimatedCounter(
             count = tdsTime.hour,
             color = color,
-            textStyle = textStyle
+            textStyle = textStyle,
+            fontSize = fontSize
         )
         TdsText(
             text = ":",
             color = color,
-            textStyle = textStyle
+            textStyle = textStyle,
+            fontSize = fontSize
         )
         TdsAnimatedCounter(
             count = tdsTime.minutes,
             color = color,
-            textStyle = textStyle
+            textStyle = textStyle,
+            fontSize = fontSize
         )
         TdsText(
             text = ":",
             color = color,
-            textStyle = textStyle
+            textStyle = textStyle,
+            fontSize = fontSize
         )
         TdsAnimatedCounter(
             count = tdsTime.seconds,
             color = color,
-            textStyle = textStyle
+            textStyle = textStyle,
+            fontSize = fontSize
         )
     }
 }
@@ -61,8 +68,9 @@ private fun TdsTimeCounterPreview() {
                 minutes = 20,
                 seconds = 20,
             ),
-            color = TiTiTheme.colors.blackColor,
-            textStyle = TiTiTheme.textStyle.normalTextStyle.copy(fontSize = 40.sp)
+            color = TdsColor.blackColor,
+            textStyle = TdsTextStyle.blackTextStyle,
+            fontSize = 40.sp
         )
     }
 }
