2c159d557ebd74e5b361d49ccf28ccddc8105649
sijin.kim <sijin.kim@yanolja.com>
2023-10-04 00:01:07 +0900

    style : unused import

--- core/designsystem/src/main/java/com/titi/designsystem/component/TdsAnimatedCounter.kt
+++ core/designsystem/src/main/java/com/titi/designsystem/component/TdsAnimatedCounter.kt
@@ -12,8 +12,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
