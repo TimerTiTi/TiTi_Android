148a686efbf6103ea402e5d893e3d1474be11ff5
koreatlwls <koreatlwls@gmail.com>
2023-10-04 00:01:07 +0900

    style : unused import

--- core/designsystem/src/main/java/com/titi/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/java/com/titi/designsystem/theme/Typography.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.titi.designsystem.R
