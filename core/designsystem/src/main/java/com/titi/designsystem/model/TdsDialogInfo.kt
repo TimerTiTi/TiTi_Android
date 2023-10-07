86e421e42616dcd3869004e9f426e0c32f9267e9
koreatlwls <koreatlwls@gmail.com>
2023-10-08 00:15:01 +0900

    feat : TdsDialogInfo model

---
+++ core/designsystem/src/main/java/com/titi/designsystem/model/TdsDialogInfo.kt
@@ -0,0 +1,30 @@
+package com.titi.designsystem.model
+
+sealed interface TdsDialogInfo {
+
+    val title: String
+    val message: String
+    val cancelable: Boolean
+    val onDismiss: (() -> Unit)?
+
+    data class Confirm(
+        override val title: String,
+        override val message: String,
+        override val cancelable: Boolean = false,
+        override val onDismiss: (() -> Unit)? = null,
+        val positiveText: String,
+        val onPositive: () -> Unit,
+        val negativeText: String,
+        val onNegative: (() -> Unit)? = null
+    ) : TdsDialogInfo
+
+    data class Alert(
+        override val title: String,
+        override val message: String,
+        override val cancelable: Boolean,
+        override val onDismiss: (() -> Unit)?,
+        val confirmText: String,
+        val onConfirm: (() -> Unit)?
+    ) : TdsDialogInfo
+
+}
