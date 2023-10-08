226ac5380379b421f5532ac8540bbb561cb4f29f
koreatlwls <koreatlwls@gmail.com>
2023-10-09 00:27:54 +0900

    feat : Long -> HH:MM:SS

--- core/designsystem/src/main/java/com/titi/designsystem/extension/LongExtensions.kt
+++ core/designsystem/src/main/java/com/titi/designsystem/extension/LongExtensions.kt
@@ -7,3 +7,13 @@ fun Long.getTdsTime() = TdsTime(
     minutes = (this % 3600 / 60).toInt(),
     seconds = (this % 60).toInt()
 )
+
+fun Long.getTimeString(): String {
+    val hour = (this / 3600).toInt()
+    val minutes = (this % 3600 / 60).toInt()
+    val seconds = (this % 60).toInt()
+
+    return "${hour.toString().padStart(2, '0')}:${
+        minutes.toString().padStart(2, '0')
+    }:${seconds.toString().padStart(2, '0')}"
+}
