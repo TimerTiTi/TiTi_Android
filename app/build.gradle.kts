1e6fc5422b862b056cfa52e2b21e570e0990a952
koreatlwls <koreatlwls@gmail.com>
2023-10-14 17:28:28 +0900

    chore : navigation 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.navigation)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
