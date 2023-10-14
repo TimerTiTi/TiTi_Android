3456b8e18396f9e5f1888cf991b772bd5030fb9a
koreatlwls <koreatlwls@gmail.com>
2023-10-14 21:41:20 +0900

    chore : 모듈 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
     implementation(project(":core:designsystem"))
+    implementation(project(":feature:time"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- feature/time/build.gradle.kts
+++ feature/time/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:designsystem"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
