bc536e2fd7ae8b77c813d67870dfd09710418f62
koreatlwls <koreatlwls@gmail.com>
2023-10-14 21:24:39 +0900

    feat : bottom navigation

--- app/src/main/java/com/titi/MainActivity.kt
+++ app/src/main/java/com/titi/MainActivity.kt
@@ -3,13 +3,116 @@ package com.titi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.titi.designsystem.theme.TdsColor
+import com.titi.designsystem.theme.TiTiTheme
+import com.titi.navigation.Screen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            TiTiTheme {
+                MainScreen(
+                    bottomNavigationBackgroundColor = TdsColor.blueColor
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun MainScreen(
+    bottomNavigationBackgroundColor: TdsColor,
+) {
+    val navController = rememberNavController()
+    val items = listOf(
+        Screen.Timer,
+        Screen.StopWatch
+    )
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = bottomNavigationBackgroundColor.getColor(),
+                tonalElevation = 0.dp
+            ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                items.forEach { screen ->
+                    NavigationBarItem(
+                        modifier = Modifier.background(bottomNavigationBackgroundColor.getColor()),
+                        label = { Text(text = screen.route) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = screen.drawableResourceId),
+                                contentDescription = screen.route,
+                            )
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = TdsColor.textColor.getColor(),
+                            selectedTextColor = TdsColor.textColor.getColor(),
+                            indicatorColor = bottomNavigationBackgroundColor.getColor(),
+                            unselectedIconColor = TdsColor.lightGrayColor.getColor(),
+                            unselectedTextColor = TdsColor.lightGrayColor.getColor()
+                        )
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = Screen.Timer.route,
+        ) {
+            composable(Screen.Timer.route) {
+                Text(text = "ABC")
+            }
+            composable(Screen.StopWatch.route) {
+                Text(text = "ABCD")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    TiTiTheme {
+        MainScreen(
+            bottomNavigationBackgroundColor = TdsColor.redColor
+        )
     }
 }
 
---
+++ app/src/main/java/com/titi/navigation/Screen.kt
@@ -0,0 +1,18 @@
+package com.titi.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.titi.designsystem.R
+
+sealed class Screen(
+    val route: String,
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val drawableResourceId: Int,
+) {
+
+    data object Timer : Screen("Timer", R.string.timer, R.drawable.timer_icon)
+
+    data object StopWatch :
+        Screen("StopWatch", R.string.stopwatch, R.drawable.stopwatch_icon)
+
+}
