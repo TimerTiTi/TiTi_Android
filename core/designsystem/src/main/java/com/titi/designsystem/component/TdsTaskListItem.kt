d363f7ae63ea282201ed8ea5cd61fd5d5d5c57e5
koreatlwls <koreatlwls@gmail.com>
2023-10-14 14:44:50 +0900

    update : swipe delete 삭제

--- core/designsystem/src/main/java/com/titi/designsystem/component/TdsTaskListItem.kt
+++ core/designsystem/src/main/java/com/titi/designsystem/component/TdsTaskListItem.kt
@@ -5,22 +5,16 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,128 +43,95 @@ fun TdsTaskListItem(
     onEdit: () -> Unit,
     onTargetTimeOn: (Boolean) -> Unit,
     onDelete: () -> Unit,
-    onLongClickMenu : () -> Unit,
+    onLongClickMenu: () -> Unit,
 ) {
-    val dismissState = rememberDismissState(confirmValueChange = { dismissValue ->
-        when (dismissValue) {
-            DismissValue.Default -> false
-            DismissValue.DismissedToEnd -> false
-            DismissValue.DismissedToStart -> {
-                onDelete()
-                true
-            }
-        }
-    })
-
-    SwipeToDismiss(
-        state = dismissState,
-        directions = setOf(DismissDirection.EndToStart),
-        background = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = TdsColor.redColor.getColor())
-                    .padding(end = 12.dp),
-                contentAlignment = Alignment.CenterEnd
-            ) {
-                TdsText(
-                    text = "Delete",
-                    textStyle = TdsTextStyle.normalTextStyle,
-                    fontSize = 16.sp,
-                    color = TdsColor.backgroundColor
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = TdsColor.backgroundColor.getColor())
+            .combinedClickable(
+                onClick = onClickTask,
+                onLongClick = onLongClickTask
+            )
+    ) {
+        Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AnimatedVisibility(visible = editMode) {
+                Icon(
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .clickable { onDelete() },
+                    painter = painterResource(id = R.drawable.cancel_icon),
+                    contentDescription = "cancel",
+                    tint = TdsColor.redColor.getColor()
                 )
             }
-        },
-        dismissContent = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = TdsColor.backgroundColor.getColor())
-                    .combinedClickable(
-                        onClick = onClickTask,
-                        onLongClick = onLongClickTask
-                    )
-            ) {
-                Row(
-                    modifier = modifier,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    AnimatedVisibility(visible = editMode) {
-                        Icon(
-                            modifier = Modifier
-                                .padding(end = 12.dp)
-                                .clickable { onDelete() },
-                            painter = painterResource(id = R.drawable.cancel_icon),
-                            contentDescription = "cancel",
-                            tint = TdsColor.redColor.getColor()
-                        )
-                    }
 
-                    Column {
+            Column {
+                TdsText(
+                    text = tdsTask.taskName,
+                    textStyle = TdsTextStyle.extraBoldTextStyle,
+                    fontSize = 20.sp,
+                    color = TdsColor.textColor,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                AnimatedVisibility(visible = tdsTask.isTaskTargetTimeOn) {
+                    Row(modifier = Modifier.padding(top = 4.dp)) {
                         TdsText(
-                            text = tdsTask.taskName,
-                            textStyle = TdsTextStyle.extraBoldTextStyle,
-                            fontSize = 20.sp,
-                            color = TdsColor.textColor,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
+                            text = stringResource(
+                                R.string.task_set_goal_time,
+                                tdsTask.taskTargetTime.getTimeString()
+                            ),
+                            textStyle = TdsTextStyle.normalTextStyle,
+                            fontSize = 14.sp,
+                            color = TdsColor.lightGrayColor
                         )
 
-                        AnimatedVisibility(visible = tdsTask.isTaskTargetTimeOn) {
-                            Row(modifier = Modifier.padding(top = 4.dp)) {
-                                TdsText(
-                                    text = stringResource(
-                                        R.string.task_set_goal_time,
-                                        tdsTask.taskTargetTime.getTimeString()
-                                    ),
-                                    textStyle = TdsTextStyle.normalTextStyle,
-                                    fontSize = 14.sp,
-                                    color = TdsColor.lightGrayColor
-                                )
+                        Spacer(modifier = Modifier.width(8.dp))
 
-                                Spacer(modifier = Modifier.width(8.dp))
-
-                                TdsText(
-                                    modifier = Modifier.clickable { onEdit() },
-                                    text = stringResource(R.string.edit),
-                                    textStyle = TdsTextStyle.normalTextStyle,
-                                    fontSize = 14.sp,
-                                    color = themeColor
-                                )
-                            }
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    Switch(
-                        checked = tdsTask.isTaskTargetTimeOn,
-                        onCheckedChange = onTargetTimeOn,
-                        colors = SwitchDefaults.colors(
-                            checkedTrackColor = themeColor.getColor(),
-                            uncheckedTrackColor = themeColor.getColor(),
-                        )
-                    )
-
-                    AnimatedVisibility(visible = editMode) {
-                        Icon(
-                            modifier = Modifier
-                                .padding(start = 12.dp)
-                                .combinedClickable(
-                                    onClick = {},
-                                    onLongClick = {onLongClickMenu()}
-                                ),
-                            painter = painterResource(id = R.drawable.menu_icon),
-                            contentDescription = "menu",
-                            tint = TdsColor.lightGrayColor.getColor()
+                        TdsText(
+                            modifier = Modifier.clickable { onEdit() },
+                            text = stringResource(R.string.edit),
+                            textStyle = TdsTextStyle.normalTextStyle,
+                            fontSize = 14.sp,
+                            color = themeColor
                         )
                     }
                 }
+            }
 
-                TdsDivider()
+            Spacer(modifier = Modifier.weight(1f))
+
+            Switch(
+                checked = tdsTask.isTaskTargetTimeOn,
+                onCheckedChange = onTargetTimeOn,
+                colors = SwitchDefaults.colors(
+                    checkedTrackColor = themeColor.getColor(),
+                    uncheckedTrackColor = themeColor.getColor(),
+                )
+            )
+
+            AnimatedVisibility(visible = editMode) {
+                Icon(
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                        .combinedClickable(
+                            onClick = {},
+                            onLongClick = { onLongClickMenu() }
+                        ),
+                    painter = painterResource(id = R.drawable.menu_icon),
+                    contentDescription = "menu",
+                    tint = TdsColor.lightGrayColor.getColor()
+                )
             }
         }
-    )
+
+        TdsDivider()
+    }
 }
 
 @Preview
