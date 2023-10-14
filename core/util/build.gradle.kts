e2ac181977ba3ac46e9a5fce088d2bf2a2e4581f
koreatlwls <koreatlwls@gmail.com>
2023-10-15 00:13:34 +0900

    chore : core:util 테스트 디펜던시 추가

--- core/util/build.gradle.kts
+++ core/util/build.gradle.kts
@@ -35,4 +35,12 @@ android {
 
 dependencies {
     implementation(libs.bundles.moshi)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
