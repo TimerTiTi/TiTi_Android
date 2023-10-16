2b89a89329597929ec9ed8436e659ad35df4df1c
koreatlwls <koreatlwls@gmail.com>
2023-10-16 23:53:40 +0900

    feat : TdsIconButton

--- core/designsystem/src/main/kotlin/com/titi/core/designsystem/component/TdsButton.kt
+++ core/designsystem/src/main/kotlin/com/titi/core/designsystem/component/TdsButton.kt
@@ -1,16 +1,27 @@
 package com.titi.core.designsystem.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,6 +60,35 @@ fun TdsTextButton(
         )
     }
 
+}
+
+@Composable
+fun TdsIconButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    size: Dp = 32.dp,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Button,
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = size / 2
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
 }
 
 @Preview
