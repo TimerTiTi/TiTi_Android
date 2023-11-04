5c5b3eb4bd59e9852c4aff9a9382516094bcf23b
koreatlwls <koreatlwls@gmail.com>
2023-11-04 17:46:41 +0900

    #15 feat : colorrepository module

---
+++ data/color/impl/src/main/kotlin/com/titi/data/color/impl/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.titi.data.color.impl.di
+
+import com.titi.data.color.api.ColorRepository
+import com.titi.data.color.impl.repository.ColorRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface RepositoryModule {
+
+    @Binds
+    @Singleton
+    fun provideColorRepository(
+        colorRepositoryImpl: ColorRepositoryImpl
+    ): ColorRepository
+
+}
