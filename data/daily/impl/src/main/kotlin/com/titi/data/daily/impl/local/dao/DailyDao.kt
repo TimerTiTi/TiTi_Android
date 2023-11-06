12b11bf96040d0c3db8da2ca37f45d7e6b8edad2
koreatlwls <koreatlwls@gmail.com>
2023-11-06 19:58:14 +0900

    #18 feat : daily dao, db module 추가

---
+++ data/daily/impl/src/main/kotlin/com/titi/data/daily/impl/local/dao/DailyDao.kt
@@ -0,0 +1,13 @@
+package com.titi.data.daily.impl.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.titi.data.daily.impl.local.model.DailyEntity
+
+@Dao
+internal interface DailyDao {
+
+    @Query("SELECT * FROM dailies ORDER BY id DESC LIMIT 1")
+    suspend fun getDaily() : DailyEntity
+
+}
