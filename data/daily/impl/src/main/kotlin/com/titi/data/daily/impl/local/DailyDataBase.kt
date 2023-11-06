7b4a5db35141eed2d32cb64b99289a153a2b0640
koreatlwls <koreatlwls@gmail.com>
2023-11-06 20:23:49 +0900

    #18 feat : daily db converter annotation 추가

--- data/daily/impl/src/main/kotlin/com/titi/data/daily/impl/local/DailyDataBase.kt
+++ data/daily/impl/src/main/kotlin/com/titi/data/daily/impl/local/DailyDataBase.kt
@@ -2,6 +2,10 @@ package com.titi.data.daily.impl.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.titi.data.daily.impl.local.converter.ListConverter
+import com.titi.data.daily.impl.local.converter.MapConverter
+import com.titi.data.daily.impl.local.converter.MapListConverter
 import com.titi.data.daily.impl.local.dao.DailyDao
 import com.titi.data.daily.impl.local.model.DailyEntity
 
@@ -9,6 +13,11 @@ import com.titi.data.daily.impl.local.model.DailyEntity
     entities = [DailyEntity::class],
     version = 1,
     exportSchema = false
+)
+@TypeConverters(
+    ListConverter::class,
+    MapConverter::class,
+    MapListConverter::class
 )
 internal abstract class DailyDataBase : RoomDatabase() {
 
