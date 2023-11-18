28141dd7a5253ac4c79c4642c57a83dfbc27c281
koreatlwls <koreatlwls@gmail.com>
2023-11-18 22:26:39 +0900

    #20 feat : 스탑워치 초기화 유스케이스 구현

---
+++ domain/time/src/main/kotlin/com/titi/domain/time/usecase/UpdateSavedStopWatchTimeUseCase.kt
@@ -0,0 +1,19 @@
+package com.titi.domain.time.usecase
+
+import com.titi.data.time.api.RecordTimesRepository
+import com.titi.domain.time.mapper.toRepositoryModel
+import com.titi.domain.time.model.RecordTimes
+import javax.inject.Inject
+
+class UpdateSavedStopWatchTimeUseCase @Inject constructor(
+    private val recordTimesRepository: RecordTimesRepository
+) {
+
+    suspend operator fun invoke(recordTimes: RecordTimes) {
+        recordTimesRepository.setRecordTimes(
+            recordTimes.toRepositoryModel()
+                .copy(savedStopWatchTime = 0)
+        )
+    }
+
+}
