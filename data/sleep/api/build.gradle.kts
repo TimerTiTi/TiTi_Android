d558aa33a2f6e9e8d5eea1a2126791b2793579eb
koreatlwls <koreatlwls@gmail.com>
2023-11-29 23:29:55 +0900

    #24 chore : data:sleep 모듈 추가

---
+++ data/sleep/api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("titi.android.library")
+}
+
+android {
+    namespace = "com.daangn.data.sleep.api"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
---
+++ data/sleep/impl/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("titi.android.library")
+    id("titi.android.hilt")
+}
+
+android {
+    namespace = "com.daangn.data.sleep.impl"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
+
+dependencies {
+    implementation(project(":data:sleep:api"))
+    implementation(project(":core:util"))
+
+    implementation(libs.bundles.moshi)
+    implementation(libs.bundles.datastore)
+}
