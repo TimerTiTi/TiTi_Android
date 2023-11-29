692d03fb03699a3981d349b72d4771c5cdbd564a
koreatlwls <koreatlwls@gmail.com>
2023-11-29 23:51:44 +0900

    #24 feat : sleep usecase 작성

---
+++ domain/sleep/src/main/kotlin/com/titi/domain/sleep/SetSleepModeUseCase.kt
@@ -0,0 +1,14 @@
+package com.titi.domain.sleep
+
+import com.titi.data.sleep.api.SleepRepository
+import javax.inject.Inject
+
+class SetSleepModeUseCase @Inject constructor(
+    private val sleepRepository: SleepRepository
+) {
+
+    suspend operator fun invoke(isSleep: Boolean) {
+        sleepRepository.setSleep(isSleep)
+    }
+
+}
