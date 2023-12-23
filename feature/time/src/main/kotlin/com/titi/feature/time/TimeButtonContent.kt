cc3563783af32e4c4660c0f305ac6b06adc83d29
koreatlwls <koreatlwls@gmail.com>
2023-12-23 19:05:49 +0900

    #26 feat : TimeButtonContent

---
+++ feature/time/src/main/kotlin/com/titi/feature/time/TimeButtonContent.kt
@@ -0,0 +1,91 @@
+package com.titi.feature.time
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.titi.core.designsystem.component.TdsIconButton
+import com.titi.core.designsystem.theme.TdsColor
+import com.titi.designsystem.R
+
+@Composable
+fun TimeButtonContent(
+    recordingMode: Int,
+    isDailyAfter6AM: Boolean,
+    onClickAddDaily: () -> Unit,
+    onClickStartRecord: () -> Unit,
+    onClickSettingTimer: (() -> Unit)? = null,
+    onClickResetStopwatch: (() -> Unit)? = null,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TdsIconButton(
+            onClick = onClickAddDaily,
+            size = 50.dp
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.add_record_icon),
+                contentDescription = "addRecord",
+                tint = if (isDailyAfter6AM) {
+                    Color.Unspecified
+                } else {
+                    TdsColor.redColor.getColor()
+                }
+            )
+        }
+
+        Spacer(modifier = Modifier.width(25.dp))
+
+        TdsIconButton(
+            onClick = onClickStartRecord,
+            size = 70.dp
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.start_record_icon),
+                contentDescription = "startRecord",
+                tint = Color.Unspecified
+            )
+        }
+
+        Spacer(modifier = Modifier.width(25.dp))
+
+        if (recordingMode == 1) {
+            TdsIconButton(
+                onClick = { onClickSettingTimer?.invoke() },
+                size = 50.dp
+            ) {
+                Icon(
+                    painter = painterResource(
+                        R.drawable.setting_timer_time_icon
+                    ),
+                    contentDescription = "",
+                    tint = Color.Unspecified
+                )
+            }
+        } else {
+            TdsIconButton(
+                onClick = { onClickResetStopwatch?.invoke() },
+                size = 50.dp
+            ) {
+                Icon(
+                    painter = painterResource(
+                        R.drawable.setting_stopwatch_time_icon
+                    ),
+                    contentDescription = "",
+                    tint = Color.Unspecified
+                )
+            }
+        }
+    }
+}
