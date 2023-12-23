40bf16f7e1d8b9386bf61216b8baa429654631d9
koreatlwls <koreatlwls@gmail.com>
2023-12-23 16:14:25 +0900

    #26 chore : core:ui

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("titi.android.library")
+}
+
+android {
+    namespace = "com.titi.core.ui"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
+
+dependencies {
+    
+}
