4c09749ef9637a7a405520410377b79d7a9dba95
koreatlwls <koreatlwls@gmail.com>
2023-12-23 15:46:46 +0900

    #26 remove : 불필요한 parcelize 삭제

--- app/src/main/kotlin/com/titi/SplashResultState.kt
+++ app/src/main/kotlin/com/titi/SplashResultState.kt
@@ -1,12 +1,9 @@
 package com.titi
 
-import android.os.Parcelable
 import com.titi.domain.color.model.TimeColor
 import com.titi.domain.time.model.RecordTimes
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class SplashResultState(
     val recordTimes: RecordTimes = RecordTimes(),
     val timeColor: TimeColor = TimeColor()
-) : Parcelable
+)
