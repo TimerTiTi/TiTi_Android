a74aca83055bf7f9b76aba62866c3db04130ceba
koreatlwls <koreatlwls@gmail.com>
2023-12-23 19:05:59 +0900

    #26 feat : TimeColorDialog

---
+++ feature/time/src/main/kotlin/com/titi/feature/time/TimeColorDialog.kt
@@ -0,0 +1,54 @@
+package com.titi.feature.time
+
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.titi.core.designsystem.component.TdsDialog
+import com.titi.core.designsystem.model.TdsDialogInfo
+import com.titi.designsystem.R
+import com.titi.feature.time.content.ColorSelectContent
+import com.titi.feature.time.ui.color.ColorActivity
+
+@Composable
+fun TimeColorDialog(
+    backgroundColor: Color,
+    textColor: Color,
+    recordingMode: Int,
+    onNegative: () -> Unit,
+    onShowDialog: (Boolean) -> Unit,
+    onClickTextColor: (Boolean) -> Unit,
+) {
+    val context = LocalContext.current
+
+    TdsDialog(
+        modifier = Modifier.background(color = Color(0xCCFFFFFF)),
+        tdsDialogInfo = TdsDialogInfo.Confirm(
+            title = stringResource(id = R.string.custom_color),
+            positiveText = stringResource(id = R.string.Ok),
+            negativeText = stringResource(id = R.string.Cancel),
+            onPositive = {},
+            onNegative = onNegative,
+        ),
+        onShowDialog = onShowDialog
+    ) {
+        ColorSelectContent(
+            backgroundColor = backgroundColor,
+            textColor = textColor,
+            onClickBackgroundColor = {
+                context.startActivity(
+                    Intent(
+                        context,
+                        ColorActivity::class.java
+                    ).apply {
+                        putExtra(ColorActivity.RECORDING_MODE_KEY, recordingMode)
+                    }
+                )
+            },
+            onClickTextColor = onClickTextColor
+        )
+    }
+}
