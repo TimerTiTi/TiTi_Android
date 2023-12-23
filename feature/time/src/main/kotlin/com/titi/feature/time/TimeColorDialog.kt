d67b09967cd53d26f0061902a147e5f54595aa0d
koreatlwls <koreatlwls@gmail.com>
2023-12-23 23:57:04 +0900

    #26 update : spacer 추가

--- feature/time/src/main/kotlin/com/titi/feature/time/TimeColorDialog.kt
+++ feature/time/src/main/kotlin/com/titi/feature/time/TimeColorDialog.kt
@@ -2,11 +2,14 @@ package com.titi.feature.time
 
 import android.content.Intent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.titi.core.designsystem.component.TdsDialog
 import com.titi.core.designsystem.model.TdsDialogInfo
 import com.titi.designsystem.R
@@ -50,5 +53,6 @@ fun TimeColorDialog(
             },
             onClickTextColor = onClickTextColor
         )
+        Spacer(Modifier.height(16.dp))
     }
 }
