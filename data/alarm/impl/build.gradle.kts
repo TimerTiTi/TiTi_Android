212a1719820025fdb86c1fdd45750f59a41fb73e
koreatlwls <koreatlwls@gmail.com>
2024-01-01 20:09:28 +0900

    #28 chore : data:alarm:impl 모듈 추가

---
+++ data/alarm/impl/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("titi.android.library")
+    id("titi.android.hilt")
+}
+
+android {
+    namespace = "com.titi.data.alarm.impl"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
+
+dependencies {
+    implementation(project(":core:util"))
+    implementation(project(":data:alarm:api"))
+
+    implementation(libs.bundles.moshi)
+    implementation(libs.bundles.datastore)
+}
