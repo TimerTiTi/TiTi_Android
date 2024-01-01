186809aaa320cf258e4e7fdc30be4aebdd5355ec
koreatlwls <koreatlwls@gmail.com>
2024-01-01 20:05:20 +0900

    #28 chore : data:alarm:api 모듈 추가

---
+++ data/alarm/api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("titi.android.library")
+}
+
+android {
+    namespace = "com.titi.data.alarm.api"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
