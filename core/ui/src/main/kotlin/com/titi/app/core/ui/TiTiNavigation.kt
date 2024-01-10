f26d79471117b47d41280d20d83efe0053753e01
koreatlwls <koreatlwls@gmail.com>
2024-01-10 17:45:26 +0900

    #30 Style : 불필요한 navigation 상수 제거

--- core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiNavigation.kt
+++ core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiNavigation.kt
@@ -1,19 +1,9 @@
 package com.titi.app.core.ui
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.navigation.NavController
-import androidx.navigation.NavOptionsBuilder
-import com.titi.app.core.designsystem.R
-import com.titi.app.core.ui.TiTiArgs.MAIN_FINISH_ARG
-import com.titi.app.core.ui.TiTiArgs.MAIN_SPLASH_ARG
-import com.titi.app.core.ui.TiTiArgs.MAIN_START_ARG
-import com.titi.app.core.ui.TiTiScreens.MAIN_SCREEN
 import com.titi.app.core.ui.TiTiScreens.SPLASH_SCREEN
 
 object TiTiScreens {
     const val SPLASH_SCREEN = "splash"
-    const val MAIN_SCREEN = "main"
 }
 
 object TiTiArgs {
@@ -24,39 +14,4 @@ object TiTiArgs {
 
 object TiTiDestinations {
     const val SPLASH_ROUTE = SPLASH_SCREEN
-    const val MAIN_ROUTE =
-        "$MAIN_SCREEN?$MAIN_START_ARG={$MAIN_START_ARG}&$MAIN_SPLASH_ARG={$MAIN_SPLASH_ARG}&$MAIN_FINISH_ARG={$MAIN_FINISH_ARG}"
-}
-
-sealed class TiTiBottomNavigationScreen(
-    val route: String,
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val drawableResourceId: Int,
-) {
-
-    data object Timer : TiTiBottomNavigationScreen("timer", R.string.timer, R.drawable.timer_icon)
-
-    data object StopWatch :
-        TiTiBottomNavigationScreen("stopwatch", R.string.stopwatch, R.drawable.stopwatch_icon)
-
-}
-
-fun NavOptionsBuilder.popUpToTop(navController: NavController) {
-    popUpTo(navController.graph.id) {
-        inclusive = true
-    }
-}
-
-class TiTiNavigationActions(private val navController: NavController) {
-
-    fun navigateToMain(
-        startDestination: Int,
-        splashScreenResult: String? = null,
-        isFinish: Boolean = false
-    ) {
-        navController.navigate("$MAIN_SCREEN?$MAIN_START_ARG=$startDestination&$MAIN_SPLASH_ARG=$splashScreenResult&$MAIN_FINISH_ARG=$isFinish") {
-            popUpToTop(navController)
-        }
-    }
-
 }
