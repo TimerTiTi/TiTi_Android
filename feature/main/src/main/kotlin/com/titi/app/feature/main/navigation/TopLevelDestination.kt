4f41fbc76fab30b857335456b436fa764c7785b7
koreatlwls <koreatlwls@gmail.com>
2024-01-10 11:35:00 +0900

    #30 Add : TopLevelDestination

---
+++ feature/main/src/main/kotlin/com/titi/app/feature/main/navigation/TopLevelDestination.kt
@@ -0,0 +1,19 @@
+package com.titi.app.feature.main.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.titi.app.core.designsystem.R
+
+enum class TopLevelDestination(
+    @StringRes val titleTextId: Int,
+    @DrawableRes val iconResourceId: Int,
+) {
+    TIMER(
+        titleTextId = R.string.timer,
+        iconResourceId = R.drawable.timer_icon
+    ),
+    STOPWATCH(
+        titleTextId = R.string.stopwatch,
+        iconResourceId = R.drawable.stopwatch_icon
+    )
+}
