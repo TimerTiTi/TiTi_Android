f7c0db8c4166567590bbda0540a8b0007bc4c5e4
koreatlwls <koreatlwls@gmail.com>
2024-01-10 20:15:08 +0900

    #30 style : 불필요한 navigation 상수 제거

--- core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiNavigation.kt
+++ core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiNavigation.kt
@@ -1,17 +1,7 @@
 package com.titi.app.core.ui
 
-import com.titi.app.core.ui.TiTiScreens.SPLASH_SCREEN
-
-object TiTiScreens {
-    const val SPLASH_SCREEN = "splash"
-}
-
 object TiTiArgs {
     const val MAIN_START_ARG = "startDestination"
     const val MAIN_SPLASH_ARG = "splashScreenResult"
     const val MAIN_FINISH_ARG = "isFinish"
-}
-
-object TiTiDestinations {
-    const val SPLASH_ROUTE = SPLASH_SCREEN
 }
