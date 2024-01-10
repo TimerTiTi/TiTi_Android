c28f667e8f4c87dd0c66b823b459f21bfe1a7b75
koreatlwls <koreatlwls@gmail.com>
2024-01-10 22:55:17 +0900

    #30 style : 불필요한 네비게이션 삭제

--- core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiDeepLink.kt
+++ core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiDeepLink.kt
@@ -3,9 +3,7 @@ package com.titi.app.core.ui
 import android.net.Uri
 import androidx.core.net.toUri
 import com.titi.app.core.ui.TiTiDeepLinkArgs.COLOR_ARG
-import com.titi.app.core.ui.TiTiDeepLinkArgs.MEASURE_ARG
 import com.titi.app.core.ui.TiTiDeepLinkConfigs.HOST_COLOR
-import com.titi.app.core.ui.TiTiDeepLinkConfigs.HOST_MEASURE
 import com.titi.app.core.ui.TiTiDeepLinkConfigs.SCHEME
 
 object TiTiDeepLinkConfigs {
@@ -13,22 +11,16 @@ object TiTiDeepLinkConfigs {
     const val SCHEME = "titi"
 
     const val HOST_COLOR = "color"
-    const val HOST_MEASURE = "measure"
 
 }
 
 object TiTiDeepLinkArgs {
     const val COLOR_ARG = "recordingMode"
-    const val MEASURE_ARG = "splashResultState"
 }
 
 enum class TiTiDeepLink(val uri: Uri) {
-    COLOR("$SCHEME://$HOST_COLOR/".toUri()),
-    MEASURE("$SCHEME://$HOST_MEASURE/".toUri())
+    COLOR("$SCHEME://$HOST_COLOR/".toUri())
 }
 
 fun createColorUri(recordingMode: Int) =
     TiTiDeepLink.COLOR.uri.toString().plus("?$COLOR_ARG=$recordingMode").toUri()
-
-fun createMeasureUri(splashResultState: String) =
-    TiTiDeepLink.MEASURE.uri.toString().plus("?$MEASURE_ARG=$splashResultState").toUri()
--- core/ui/src/main/kotlin/com/titi/app/core/ui/TiTiNavigation.kt
+++
@@ -1,7 +0,0 @@
-package com.titi.app.core.ui
-
-object TiTiArgs {
-    const val MAIN_START_ARG = "startDestination"
-    const val MAIN_SPLASH_ARG = "splashScreenResult"
-    const val MAIN_FINISH_ARG = "isFinish"
-}
