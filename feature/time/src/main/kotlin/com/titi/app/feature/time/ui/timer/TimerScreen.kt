1ec03c759a241e25abb5fe1c5156921929c5dac0
koreatlwls <koreatlwls@gmail.com>
2024-02-02 22:25:41 +0900

    #39 feat : 타이머, 스탑워치 가로모드 화면 구현

--- feature/time/src/main/kotlin/com/titi/app/feature/time/ui/timer/TimerScreen.kt
+++ feature/time/src/main/kotlin/com/titi/app/feature/time/ui/timer/TimerScreen.kt
@@ -1,14 +1,19 @@
 package com.titi.app.feature.time.ui.timer
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.airbnb.mvrx.asMavericksArgs
@@ -25,6 +31,7 @@ import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
 import com.titi.app.core.designsystem.R
 import com.titi.app.core.designsystem.component.TdsTimer
+import com.titi.app.core.designsystem.extension.times
 import com.titi.app.core.designsystem.theme.TdsColor
 import com.titi.app.core.util.toJson
 import com.titi.app.feature.time.SplashResultState
@@ -214,68 +221,115 @@ private fun TimerScreen(
     onClickStartRecord: () -> Unit,
     onClickSettingTimer: () -> Unit,
 ) {
-    val scrollState = rememberScrollState()
+    val configuration = LocalConfiguration.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(uiState.timerColor.backgroundColor))
-            .padding(top = 16.dp)
-            .verticalScroll(scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        TimeHeaderContent(
-            todayDate = uiState.todayDate,
-            isDailyAfter6AM = uiState.isDailyAfter6AM,
-            textColor = textColor,
-            onClickColor = onClickColor,
-        )
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(uiState.timerColor.backgroundColor))
+                    .padding(top = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                TimeHeaderContent(
+                    todayDate = uiState.todayDate,
+                    isDailyAfter6AM = uiState.isDailyAfter6AM,
+                    textColor = textColor,
+                    onClickColor = onClickColor,
+                )
 
-        Spacer(modifier = Modifier.weight(1f))
+                Spacer(modifier = Modifier.weight(1f))
 
-        TimeTaskContent(
-            isSetTask = uiState.isSetTask,
-            textColor = textColor,
-            taskName = uiState.taskName,
-            onClickTask = onClickTask,
-        )
+                TimeTaskContent(
+                    isSetTask = uiState.isSetTask,
+                    textColor = textColor,
+                    taskName = uiState.taskName,
+                    onClickTask = onClickTask,
+                )
 
-        Spacer(modifier = Modifier.height(50.dp))
+                Spacer(modifier = Modifier.height(50.dp))
 
-        with(uiState.timerRecordTimes) {
-            TdsTimer(
-                modifier = Modifier.clickable {
-                    onClickStartRecord()
-                },
-                isFinish = isFinish,
-                outCircularLineColor = textColor.getColor(),
-                outCircularProgress = outCircularProgress,
-                inCircularLineTrackColor = if (textColor == TdsColor.WHITE) {
-                    TdsColor.BLACK
-                } else {
-                    TdsColor.WHITE
-                },
-                inCircularProgress = inCircularProgress,
-                fontColor = textColor,
-                recordingMode = 1,
-                savedSumTime = savedSumTime,
-                savedTime = savedTime,
-                savedGoalTime = savedGoalTime,
-                finishGoalTime = finishGoalTime,
-                isTaskTargetTimeOn = isTaskTargetTimeOn,
-            )
+                with(uiState.timerRecordTimes) {
+                    TdsTimer(
+                        modifier = Modifier.clickable {
+                            onClickStartRecord()
+                        },
+                        isFinish = isFinish,
+                        outCircularLineColor = textColor.getColor(),
+                        outCircularProgress = outCircularProgress,
+                        inCircularLineTrackColor = if (textColor == TdsColor.WHITE) {
+                            TdsColor.BLACK
+                        } else {
+                            TdsColor.WHITE
+                        },
+                        inCircularProgress = inCircularProgress,
+                        fontColor = textColor,
+                        recordingMode = 1,
+                        savedSumTime = savedSumTime,
+                        savedTime = savedTime,
+                        savedGoalTime = savedGoalTime,
+                        finishGoalTime = finishGoalTime,
+                        isTaskTargetTimeOn = isTaskTargetTimeOn,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                TimeButtonContent(
+                    recordingMode = 1,
+                    isDailyAfter6AM = uiState.isDailyAfter6AM,
+                    onClickAddDaily = onClickAddDaily,
+                    onClickStartRecord = onClickStartRecord,
+                    onClickSettingTimer = onClickSettingTimer,
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+            }
         }
 
-        Spacer(modifier = Modifier.height(50.dp))
+        else -> {
+            BoxWithConstraints(
+                Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding()
+                    .background(Color(uiState.timerColor.backgroundColor)),
+            ) {
+                val maxWidth = maxWidth
 
-        TimeButtonContent(
-            recordingMode = 1,
-            isDailyAfter6AM = uiState.isDailyAfter6AM,
-            onClickAddDaily = onClickAddDaily,
-            onClickStartRecord = onClickStartRecord,
-            onClickSettingTimer = onClickSettingTimer,
-        )
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(modifier = Modifier.size(maxWidth.times(0.2)))
 
-        Spacer(modifier = Modifier.weight(1f))
+                    with(uiState.timerRecordTimes) {
+                        TdsTimer(
+                            modifier = Modifier
+                                .clickable {
+                                    onClickStartRecord()
+                                },
+                            isFinish = isFinish,
+                            outCircularLineColor = textColor.getColor(),
+                            outCircularProgress = outCircularProgress,
+                            inCircularLineTrackColor = if (textColor == TdsColor.WHITE) {
+                                TdsColor.BLACK
+                            } else {
+                                TdsColor.WHITE
+                            },
+                            inCircularProgress = inCircularProgress,
+                            fontColor = textColor,
+                            recordingMode = 1,
+                            savedSumTime = savedSumTime,
+                            savedTime = savedTime,
+                            savedGoalTime = savedGoalTime,
+                            finishGoalTime = finishGoalTime,
+                            isTaskTargetTimeOn = isTaskTargetTimeOn,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
