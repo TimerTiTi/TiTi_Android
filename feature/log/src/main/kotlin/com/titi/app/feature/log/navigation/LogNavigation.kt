dee400eb53da832ca28c3b5977d3bbc8e76ec08f
koreatlwls <koreatlwls@gmail.com>
2024-02-07 21:46:37 +0900

    #50 feat : LogNavigation

---
+++ feature/log/src/main/kotlin/com/titi/app/feature/log/navigation/LogNavigation.kt
@@ -0,0 +1,29 @@
+package com.titi.app.feature.log.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.titi.app.feature.log.LogScreen
+
+private const val LOG_GRAPH_SCREEN = "logGraph"
+const val LOG_GRAPH_ROUTE = LOG_GRAPH_SCREEN
+
+private const val LOG_SCREEN = "log"
+const val LOG_ROUTE = LOG_SCREEN
+
+fun NavController.navigateToLog(navOptions: NavOptions) {
+    navigate(LOG_ROUTE, navOptions)
+}
+
+fun NavGraphBuilder.logGraph() {
+    navigation(
+        route = LOG_GRAPH_ROUTE,
+        startDestination = LOG_ROUTE,
+    ) {
+        composable(route = LOG_ROUTE) {
+            LogScreen()
+        }
+    }
+}
