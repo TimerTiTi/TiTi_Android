4c998e39de83e162867ea3b3e8c4570e85f73b37
koreatlwls <koreatlwls@gmail.com>
2024-02-08 19:12:32 +0900

    #50 feat : 달력

--- feature/log/src/main/kotlin/com/titi/app/feature/log/ui/DailyScreen.kt
+++ feature/log/src/main/kotlin/com/titi/app/feature/log/ui/DailyScreen.kt
@@ -1,33 +1,55 @@
 package com.titi.app.feature.log.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.OutDateStyle
+import com.kizitonwose.calendar.core.daysOfWeek
 import com.titi.app.core.designsystem.R
 import com.titi.app.core.designsystem.component.TdsColorRow
 import com.titi.app.core.designsystem.component.TdsIconButton
@@ -42,6 +64,11 @@ import com.titi.app.core.designsystem.model.TdsTimeTableData
 import com.titi.app.core.designsystem.theme.TdsColor
 import com.titi.app.core.designsystem.theme.TdsTextStyle
 import com.titi.app.core.designsystem.theme.TiTiTheme
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
 import kotlinx.coroutines.launch
 
 @Composable
@@ -85,26 +112,36 @@ fun DailyScreen(
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CalendarContent(modifier: Modifier = Modifier) {
-    val pagerState = rememberPagerState(
-        pageCount = {
-            4
-        },
-    )
     val scope = rememberCoroutineScope()
+
+    val currentMonth = remember { YearMonth.now() }
+    val startMonth = remember { currentMonth.minusMonths(100) }
+    val endMonth = remember { currentMonth.plusMonths(100) }
+    val daysOfWeek = remember { daysOfWeek() }
+
+    val state = rememberCalendarState(
+        startMonth = startMonth,
+        endMonth = endMonth,
+        firstVisibleMonth = currentMonth,
+        firstDayOfWeek = daysOfWeek.first(),
+        outDateStyle = OutDateStyle.EndOfGrid,
+    )
 
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
     ) {
+        val visibleYear = state.firstVisibleMonth.yearMonth.year
+        val visibleMonth = state.firstVisibleMonth.yearMonth.monthValue
+
         TdsIconButton(
             onClick = {
+                val previousYearMonth = state.firstVisibleMonth.yearMonth.minusMonths(1)
+
                 scope.launch {
-                    if (pagerState.currentPage - 1 >= 0) {
-                        pagerState.animateScrollToPage(pagerState.currentPage - 1)
-                    }
+                    state.animateScrollToMonth(previousYearMonth)
                 }
             },
         ) {
@@ -115,49 +152,76 @@ fun CalendarContent(modifier: Modifier = Modifier) {
             )
         }
 
-        HorizontalPager(
-            modifier = Modifier.weight(1f),
-            userScrollEnabled = true,
-            state = pagerState,
-        ) { page ->
-            BoxWithConstraints(
-                modifier = modifier.padding(vertical = 10.dp),
-                contentAlignment = Alignment.Center,
-            ) {
-                val size = if (maxWidth >= 365.dp) 345.dp else maxWidth - 20.dp
+        BoxWithConstraints(
+            modifier = modifier
+                .weight(1f)
+                .padding(vertical = 10.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            val size = if (maxWidth >= 365.dp) 345.dp else maxWidth - 20.dp
 
-                Card(
+            Card(
+                modifier = Modifier
+                    .width(size)
+                    .wrapContentHeight(),
+                shape = RoundedCornerShape(25.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = TdsColor.BACKGROUND.getColor(),
+                ),
+                elevation = CardDefaults.outlinedCardElevation(defaultElevation = 5.dp),
+            ) {
+                Column(
                     modifier = Modifier
-                        .size(size),
-                    shape = RoundedCornerShape(25.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = TdsColor.BACKGROUND.getColor(),
-                    ),
-                    elevation = CardDefaults.outlinedCardElevation(defaultElevation = 5.dp),
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(10.dp),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        TdsText(
-                            text = "달력",
-                            textStyle = TdsTextStyle.SEMI_BOLD_TEXT_STYLE,
-                            fontSize = 40.sp,
-                            color = TdsColor.TEXT,
-                        )
-                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    TdsText(
+                        text = "$visibleYear ${
+                            visibleMonth.toString().padStart(2, '0')
+                        }",
+                        textStyle = TdsTextStyle.EXTRA_BOLD_TEXT_STYLE,
+                        fontSize = 24.sp,
+                        color = TdsColor.D1,
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    DaysOfWeekTitle(
+                        daysOfWeek,
+                        TdsColor.D1,
+                    )
+
+                    var selectedDate by remember { mutableStateOf<LocalDate>(LocalDate.now()) }
+
+                    HorizontalCalendar(
+                        state = state,
+                        dayContent = { day ->
+                            Day(
+                                day = day,
+                                isSelected = selectedDate == day.date,
+                                themeColor = TdsColor.D1,
+                            ) { selectedDay ->
+                                selectedDate = if (selectedDate == selectedDay.date) {
+                                    selectedDate
+                                } else {
+                                    selectedDay.date
+                                }
+                            }
+                        },
+                    )
                 }
             }
         }
 
         TdsIconButton(
             onClick = {
+                val nextYearMonth = state.firstVisibleMonth.yearMonth.plusMonths(1)
+
                 scope.launch {
-                    if (pagerState.currentPage + 1 < 4) {
-                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
-                    }
+                    state.animateScrollToMonth(nextYearMonth)
                 }
             },
         ) {
@@ -165,6 +229,62 @@ fun CalendarContent(modifier: Modifier = Modifier) {
                 imageVector = ImageVector.vectorResource(R.drawable.arrow_right_icon),
                 contentDescription = "arrowRight",
                 tint = TdsColor.TEXT.getColor(),
+            )
+        }
+    }
+}
+
+@Composable
+fun Day(
+    day: CalendarDay,
+    isSelected: Boolean,
+    themeColor: TdsColor,
+    onClick: (CalendarDay) -> Unit,
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .clickable(
+                enabled = day.position == DayPosition.MonthDate,
+                onClick = { onClick(day) },
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(min(maxWidth, maxHeight) / 5 * 3)
+                .clip(CircleShape)
+                .background(
+                    color = if (isSelected) {
+                        themeColor.getColor()
+                    } else {
+                        Color.Transparent
+                    },
+                ),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = day.date.dayOfMonth.toString(),
+                color = if (day.position == DayPosition.MonthDate) {
+                    TdsColor.TEXT.getColor()
+                } else {
+                    Color.Gray
+                },
+                style = TdsTextStyle.SEMI_BOLD_TEXT_STYLE.getTextStyle(fontSize = 18.sp),
+            )
+        }
+    }
+}
+
+@Composable
+fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>, themeColor: TdsColor) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                color = themeColor.getColor(),
             )
         }
     }
