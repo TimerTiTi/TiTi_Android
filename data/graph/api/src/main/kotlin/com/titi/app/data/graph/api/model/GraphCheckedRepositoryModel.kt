3dc52f4cd3b0c2e343de3de0cccff14360fc3db4
koreatlwls <koreatlwls@gmail.com>
2024-02-28 00:02:59 +0900

    #72 feat : GraphCheckedRepositoryModel

---
+++ data/graph/api/src/main/kotlin/com/titi/app/data/graph/api/model/GraphCheckedRepositoryModel.kt
@@ -0,0 +1,8 @@
+package com.titi.app.data.graph.api.model
+
+data class GraphCheckedRepositoryModel(
+    val firstChecked: Boolean,
+    val secondChecked: Boolean,
+    val thirdChecked: Boolean,
+    val fourthChecked: Boolean,
+)
