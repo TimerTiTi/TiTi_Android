8c14c17563de68d8ff551bf20d7750fc0641e7df
koreatlwls <koreatlwls@gmail.com>
2024-02-29 23:23:07 +0900

    #72 update : TdsToggleIconButton

--- core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/component/TdsToggleIconButton.kt
+++ core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/component/TdsToggleIconButton.kt
@@ -1,11 +1,16 @@
 package com.titi.app.core.designsystem.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -21,18 +26,26 @@ fun TdsToggleIconButton(
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
 ) {
-    Icon(
+    var isChecked by remember { mutableStateOf(checked) }
+
+    Box(
         modifier = Modifier
             .size(20.dp)
             .clip(CircleShape)
             .toggleable(
-                value = checked,
-                onValueChange = onCheckedChange,
+                value = isChecked,
+                onValueChange = {
+                    isChecked = !isChecked
+                    onCheckedChange.invoke(isChecked)
+                },
                 enabled = true,
                 role = Role.Checkbox,
             )
             .then(modifier),
-        imageVector = ImageVector.vectorResource(if (checked) checkedIcon else uncheckedIcon),
-        contentDescription = "toggle_icon_button",
-    )
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(if (isChecked) checkedIcon else uncheckedIcon),
+            contentDescription = "toggle_icon_button",
+        )
+    }
 }
