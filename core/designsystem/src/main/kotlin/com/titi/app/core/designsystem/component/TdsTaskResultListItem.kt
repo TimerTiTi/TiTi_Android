99d84a3e92a48ceb84bf79ef0e16db76d9dedcf5
koreatlwls <koreatlwls@gmail.com>
2024-03-04 22:38:08 +0900

    #70 update : taskresultitem text color

--- core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/component/TdsTaskResultListItem.kt
+++ core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/component/TdsTaskResultListItem.kt
@@ -66,7 +66,7 @@ fun TdsTaskResultListItem(
             text = taskName,
             textStyle = TdsTextStyle.SEMI_BOLD_TEXT_STYLE,
             fontSize = fontSize,
-            color = TdsColor.BACKGROUND,
+            color = TdsColor.TEXT,
             overflow = TextOverflow.Ellipsis,
             maxLines = 1,
         )
