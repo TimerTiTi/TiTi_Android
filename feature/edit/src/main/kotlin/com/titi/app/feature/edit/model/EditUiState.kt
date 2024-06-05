a86ceaa37284747e965bb768d655aa61f30837a5
koreatlwls <koreatlwls@gmail.com>
2024-06-05 17:20:45 +0900

    #80 feat : EditUiState

---
+++ feature/edit/src/main/kotlin/com/titi/app/feature/edit/model/EditUiState.kt
@@ -0,0 +1,35 @@
+package com.titi.app.feature.edit.model
+
+import com.airbnb.mvrx.MavericksState
+import com.titi.app.core.designsystem.extension.getTimeString
+import com.titi.app.core.designsystem.model.TdsTaskData
+import com.titi.app.core.designsystem.model.TdsTimeTableData
+import com.titi.app.core.designsystem.theme.TdsColor
+import java.time.LocalDate
+
+data class EditUiState(
+    val currentDate: LocalDate = LocalDate.now(),
+    val graphColors: List<TdsColor> = listOf(
+        TdsColor.D1,
+        TdsColor.D2,
+        TdsColor.D3,
+        TdsColor.D4,
+        TdsColor.D5,
+        TdsColor.D6,
+        TdsColor.D7,
+        TdsColor.D8,
+        TdsColor.D9,
+        TdsColor.D10,
+        TdsColor.D11,
+        TdsColor.D12,
+    ),
+    val dailyGraphData: DailyGraphData = DailyGraphData(),
+) : MavericksState
+
+data class DailyGraphData(
+    val totalTime: String = 0L.getTimeString(),
+    val maxTime: String = 0L.getTimeString(),
+    val timeLine: List<Long> = LongArray(24) { 0L }.toList(),
+    val taskData: List<TdsTaskData> = emptyList(),
+    val tdsTimeTableData: List<TdsTimeTableData> = emptyList(),
+)
