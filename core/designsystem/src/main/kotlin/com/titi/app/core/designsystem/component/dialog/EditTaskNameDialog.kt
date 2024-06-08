3236cecc2b6685656bf91c18db10d3e7b12045e9
koreatlwls <koreatlwls@gmail.com>
2024-06-08 23:43:04 +0900

    #80 update : extract dialog

---
+++ core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/component/dialog/EditTaskNameDialog.kt
@@ -0,0 +1,69 @@
+package com.titi.app.core.designsystem.component.dialog
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.titi.app.core.designsystem.R
+import com.titi.app.core.designsystem.component.TdsDialog
+import com.titi.app.core.designsystem.component.TdsOutlinedInputTextField
+import com.titi.app.core.designsystem.model.TdsDialogInfo
+import kotlinx.coroutines.android.awaitFrame
+
+@Composable
+fun EditTaskNameDialog(
+    taskName: TextFieldValue,
+    onPositive: (TextFieldValue) -> Unit,
+    onShowDialog: (Boolean) -> Unit,
+) {
+    var editTaskName by remember {
+        mutableStateOf(taskName)
+    }
+    val modifyTaskNameFocusRequester = remember { FocusRequester() }
+    val keyboard = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(modifyTaskNameFocusRequester) {
+        modifyTaskNameFocusRequester.requestFocus()
+        awaitFrame()
+        keyboard?.show()
+    }
+
+    TdsDialog(
+        tdsDialogInfo = TdsDialogInfo.Confirm(
+            title = stringResource(id = R.string.modify_task_title),
+            message = stringResource(id = R.string.add_task_message),
+            positiveText = stringResource(id = R.string.Ok),
+            onPositive = {
+                onPositive(editTaskName)
+            },
+            negativeText = stringResource(id = R.string.Cancel),
+        ),
+        onShowDialog = onShowDialog,
+    ) {
+        TdsOutlinedInputTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(26.dp)
+                .padding(horizontal = 15.dp)
+                .focusRequester(modifyTaskNameFocusRequester),
+            fontSize = 17.sp,
+            text = editTaskName,
+            onValueChange = {
+                editTaskName = it
+            },
+        )
+    }
+}
