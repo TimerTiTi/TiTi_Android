f3bc5de69edc14fc3f539d3ade0473b103b74e77
koreatlwls <koreatlwls@gmail.com>
2024-07-14 15:40:50 +0900

    #142 remove : unused func

--- core/util/src/main/kotlin/com/titi/app/core/util/JavaTimeUtil.kt
+++ core/util/src/main/kotlin/com/titi/app/core/util/JavaTimeUtil.kt
@@ -1,6 +1,5 @@
 package com.titi.app.core.util
 
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -9,39 +8,6 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.WeekFields
 import java.util.Locale
-
-fun isCurrentWeek(checkDate: ZonedDateTime, currentDate: LocalDate): Boolean {
-    val diffMonday = currentDate.dayOfWeek.value - DayOfWeek.MONDAY.value
-    val diffSunday = DayOfWeek.SUNDAY.value - currentDate.dayOfWeek.value
-
-    val monday = currentDate
-        .minusDays(diffMonday.toLong())
-        .atStartOfDay()
-        .atZone(ZoneOffset.systemDefault())
-        .withZoneSameInstant(ZoneOffset.UTC)
-
-    val sunday = currentDate
-        .plusDays(diffSunday.toLong())
-        .atTime(23, 59, 59)
-        .atZone(ZoneOffset.systemDefault())
-        .withZoneSameInstant(ZoneOffset.UTC)
-
-    return checkDate.isAfter(monday) && checkDate.isBefore(sunday)
-}
-
-fun isCurrentDaily(checkDate: ZonedDateTime, currentDate: LocalDate): Boolean {
-    val startCurrentDay = currentDate
-        .atStartOfDay()
-        .atZone(ZoneId.systemDefault())
-        .withZoneSameInstant(ZoneOffset.UTC)
-
-    val endCurrentDay = currentDate
-        .atTime(23, 59, 59)
-        .atZone(ZoneOffset.systemDefault())
-        .withZoneSameInstant(ZoneOffset.UTC)
-
-    return checkDate.isAfter(startCurrentDay) && checkDate.isBefore(endCurrentDay)
-}
 
 fun LocalDateTime.toOnlyTime(): String {
     val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
