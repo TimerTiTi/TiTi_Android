ab40f7dc2ca0e4d49932a438ef33f526ab2963c2
koreatlwls <koreatlwls@gmail.com>
2024-07-17 23:34:03 +0900

    #84 feat : 중국어 폰트 적용

--- core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/kotlin/com/titi/app/core/designsystem/theme/Typography.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.titi.app.core.designsystem.R
@@ -16,24 +17,27 @@ val hgggothicssiProFontFamily =
         Font(R.font.hgggothicssi_pro_60g, FontWeight.SemiBold),
         Font(R.font.hgggothicssi_pro_80g, FontWeight.ExtraBold),
     )
+val misansFontFamily =
+    FontFamily(
+        Font(R.font.misans_normal, FontWeight.Normal),
+        Font(R.font.misans_medium, FontWeight.SemiBold),
+        Font(R.font.misans_semibold, FontWeight.ExtraBold),
+    )
 
 @Immutable
 data class TdsTypography(
     val normalTextStyle: TextStyle =
         TextStyle(
-            fontFamily = hgggothicssiProFontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 1.sp,
         ),
     val semiBoldTextStyle: TextStyle =
         TextStyle(
-            fontFamily = hgggothicssiProFontFamily,
             fontWeight = FontWeight.SemiBold,
             fontSize = 1.sp,
         ),
     val extraBoldTextStyle: TextStyle =
         TextStyle(
-            fontFamily = hgggothicssiProFontFamily,
             fontWeight = FontWeight.ExtraBold,
             fontSize = 1.sp,
         ),
@@ -47,11 +51,32 @@ enum class TdsTextStyle {
 
     @Composable
     fun getTextStyle(fontSize: TextUnit) = when (this) {
-        NORMAL_TEXT_STYLE -> TiTiTheme.textStyle.normalTextStyle.copy(fontSize = fontSize)
-        SEMI_BOLD_TEXT_STYLE -> TiTiTheme.textStyle.semiBoldTextStyle.copy(fontSize = fontSize)
-        EXTRA_BOLD_TEXT_STYLE ->
+        NORMAL_TEXT_STYLE -> {
+            val fontFamily = if (Locale.current.language == "zh") {
+                misansFontFamily
+            } else {
+                hgggothicssiProFontFamily
+            }
+            TiTiTheme.textStyle.normalTextStyle.copy(fontFamily = fontFamily, fontSize = fontSize)
+        }
+        SEMI_BOLD_TEXT_STYLE -> {
+            val fontFamily = if (Locale.current.language == "zh") {
+                misansFontFamily
+            } else {
+                hgggothicssiProFontFamily
+            }
+            TiTiTheme.textStyle.semiBoldTextStyle.copy(fontFamily = fontFamily, fontSize = fontSize)
+        }
+        EXTRA_BOLD_TEXT_STYLE -> {
+            val fontFamily = if (Locale.current.language == "zh") {
+                misansFontFamily
+            } else {
+                hgggothicssiProFontFamily
+            }
             TiTiTheme.textStyle.extraBoldTextStyle.copy(
+                fontFamily = fontFamily,
                 fontSize = fontSize,
             )
+        }
     }
 }
