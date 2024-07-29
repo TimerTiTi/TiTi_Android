50ed790c1e72488fdfbe2011f67f4cc93ce88f0e
koreatlwls <koreatlwls@gmail.com>
2024-07-29 20:13:38 +0900

    #169 feat : DataStoreModule

---
+++ data/language/impl/src/main/kotlin/com/titi/app/data/language/impl/di/DataStoreModule.kt
@@ -0,0 +1,17 @@
+package com.titi.app.data.language.impl.di
+
+import android.content.Context
+import com.titi.app.data.language.impl.local.LanguageDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideLanguageDataStore(context: Context) = LanguageDataStore(context)
+}
