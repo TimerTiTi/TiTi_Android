5edd40e6d5b00b2ceb006689b679b2924b99f6f4
koreatlwls <koreatlwls@gmail.com>
2024-09-29 23:02:20 +0900

    #194 fix : navigation arguments 특수문자

--- feature/measure/src/main/kotlin/com/titi/app/feature/measure/navigation/MeasureNavigation.kt
+++ feature/measure/src/main/kotlin/com/titi/app/feature/measure/navigation/MeasureNavigation.kt
@@ -6,6 +6,8 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.titi.app.feature.measure.ui.MeasuringScreen
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 private const val MEASURE_SCREEN = "measure"
 const val MEASURE_ARG = "splashResultState"
@@ -25,7 +27,15 @@ fun NavGraphBuilder.measureGraph(onFinish: (isFinish: Boolean) -> Unit) {
         ),
     ) {
         MeasuringScreen(
-            splashResultState = it.arguments?.getString(MEASURE_ARG, "") ?: "",
+            splashResultState = it.arguments
+                ?.getString(MEASURE_ARG, "")
+                ?.let { jsonString ->
+                    URLDecoder.decode(
+                        jsonString,
+                        StandardCharsets.UTF_8.toString(),
+                    )
+                }
+                ?: "",
             onFinish = onFinish,
         )
     }
