59ae8da9c82afa27f8bfee1f3a25797487e64048
koreatlwls <koreatlwls@gmail.com>
2025-01-19 18:30:54 +0900

    #220 Update : version

--- build-logic/src/main/kotlin/com/titi/common/BuildInfo.kt
+++ build-logic/src/main/kotlin/com/titi/common/BuildInfo.kt
@@ -9,7 +9,7 @@ object BuildType {
 
 object AppConfig {
     const val APP_ID = "com.titi.app"
-    const val APP_VERSION_NAME = "1.4.10"
-    const val APP_VERSION_CODE = 52
+    const val APP_VERSION_NAME = "1.4.11"
+    const val APP_VERSION_CODE = 53
     const val APP_NAME = "TiTi"
 }
